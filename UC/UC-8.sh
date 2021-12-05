f4baae32bffa5c37329a1db3b550593a1f8df06a
Gofrieza <ankurkumar13041996@gmail.com>
2021-12-04 21:02:54 -0800

    added uc-8

---
+++ UC/UC-8.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -x
+
+isPartTime=1
+isFullTime=2
+maxHoursInMonth=100
+empRatePerHour=20
+numOfWorkingDays=20
+
+totalEmpHr=0
+totalWorkingDays=0
+
+function getWorkHrs()
+{
+	case $1 in
+			$isFullTime)
+				empHrs=8
+			;;
+			 $isPartTime)
+                                empHrs=4
+                        ;;
+			*)
+				empHrs=0
+			;;
+	esac
+}
+function getEmpWage()  {
+		echo $(($1*$empRatePerHour))
+}
+while [[ $totalEmpHr -lt $maxHoursInMonth && 
+$totalWorkingDays -lt $numOfWorkingDays ]]
+do
+        ((totalWorkingDays++))
+        empCheck=$((RANDOM%3))
+	getWorkHrs $empCheck
+        totalEmpHr=$(($totalEmpHr+$empHrs))
+	dailyWages[$totalWorkingDays]=$(($empHrs+$empRatePerHour))
+done
+totalSalary="$( getEmpWage $totalEmpHr )"
+echo ${dailyWages[@]}
